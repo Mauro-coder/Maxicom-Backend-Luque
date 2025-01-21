fa124dba11e657c5a74adf35c9871c5b9f1768b3
Mauro Luque <mauroluque72@gmail.com>
2025-01-21 10:58:02 -0300

    add handlebars

--- src/middlewares/errorHandler.mid.js
+++ src/middlewares/errorHandler.mid.js
@@ -1,13 +1,13 @@
 const errorHandler = (error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode || 500;
-    const message = error.message || "API ERROR";
-    const data = {
-      method: req.method,
-      url: req.url,
-      error: message,
-    };
-    return res.status(status).json(data);
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message || "API ERROR";
+  const data = {
+    method: req.method,
+    url: req.url,
+    error: message,
   };
-  
-  export default errorHandler;
+  return res.status(status).json(data);
+};
+
+export default errorHandler;
--- src/middlewares/pathHandler.mid.js
+++ src/middlewares/pathHandler.mid.js
@@ -1,11 +1,11 @@
 const pathHandler = (req, res) => {
-    const message = "Not found path";
-    const data = {
-      method: req.method,
-      url: req.url,
-      error: message,
-    };
-    return res.status(404).json(data);
+  const message = "Not found path";
+  const data = {
+    method: req.method,
+    url: req.url,
+    error: message,
   };
-  
-  export default pathHandler;
+  return res.status(404).json(data);
+};
+
+export default pathHandler;
